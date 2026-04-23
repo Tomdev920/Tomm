8896ad85fb1830e04e3986ec12b0347aaebefa2b
rCl <tameermohamed303@gmail.com>
2026-04-23 03:51:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-import flet as ft
+import random
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
-def main(page: ft.Page):
-    page.title = "Simple App"
-    page.add(ft.Text("Hello from Python APK 🚀"))
-    page.add(ft.ElevatedButton("Click me", on_click=lambda e: print("Clicked")))
+class Game(App):
+    def build(self):
+        self.secret = random.randint(1, 3)
 
-ft.app(target=main)
+        self.layout = BoxLayout(orientation='vertical')
+
+        self.label = Label(text="Guess number 1 to 3")
+        self.layout.add_widget(self.label)
+
+        for i in range(1, 4):
+            btn = Button(text=str(i))
+            btn.bind(on_press=self.check)
+            self.layout.add_widget(btn)
+
+        return self.layout
+
+    def check(self, instance):
+        if int(instance.text) == self.secret:
+            self.label.text = "🎉 Correct!"
+        else:
+            self.label.text = "❌ Wrong!"
+
+Game().run()
